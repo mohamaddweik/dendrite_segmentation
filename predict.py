c8be3b75375ad97f17b8e52192f858eb63564bfc
mohamad dweik <mohamad.dweik89@gmail.com>
2026-02-19 13:11:18 +0200

    trained the models

---
+++ predict.py
@@ -0,0 +1,18 @@
+from ultralytics import YOLO
+
+def main():
+    model = YOLO(r"C:\Users\moham\OneDrive\Desktop\jce\Image_processing\dendrite_segmentation\runs\segment\yolo26_dendrite_v4\weights\best.pt")
+
+    model.predict(
+        source=r"C:\Users\moham\OneDrive\Desktop\jce\Image_processing\dendrite_segmentation\dendrite_dataset\images\val",
+        imgsz=896,
+        device="cpu",
+        conf=0.15,
+        show_boxes=False,
+        show_labels=False,
+        show_conf=False,
+        save=True
+    )
+
+if __name__ == "__main__":
+    main()
--- train_yolo_dendrite_seg.py
+++ train_yolo_dendrite_seg.py
@@ -1,17 +1,18 @@
 from ultralytics import YOLO
 
-# weights of yolo26
-model = YOLO('yolo26n-seg.pt')
-
-results = model.train(
-    data='data.yaml',
-    epochs=100,
-    imgsz=640,  
-    device=0,
-    overlap_mask=True,
-    mask_ratio = 4,
-    optimize='auto',
-    project='runs/segment',
-    name='yolo26_dendrite_v1'
+def main():
+    model = YOLO('yolo26n-seg.pt')
+    model.train(
+        data='dendrite_dataset/data.yaml',
+        epochs=300,
+        batch=2,
+        imgsz=896,
+        device=0,
+        overlap_mask=True,
+        mask_ratio=4,
+        name='yolo26_dendrite_v4',
+        workers=0
     )
 
+if __name__ == "__main__":
+    main()
